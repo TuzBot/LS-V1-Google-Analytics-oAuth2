c0f58e27d69d804030f9e4ab4db152e62b861e34
Ben <ben@doginthehat.com.au>
2015-08-10 16:07:50 +1000

    Fixed error in frontend GA script output
    Should be a coma, not a colon.

--- models/cms_stats_settings.php
+++ models/cms_stats_settings.php
@@ -135,7 +135,7 @@
 			
 			$data = null;
 			foreach($plugins as $plugin=>$type)
-				$data .= "\t ga('{$type}':'{$plugin}');\n";
+				$data .= "\t ga('{$type}','{$plugin}');\n";
 					
 			$result = "\n\t<script type=\"text/javascript\">
 \t(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
